18899af9c231410d37a1385102adc7d53581fa0e
fastily <fastily@yahoo.com>
2015-02-01 21:58:16 -0800

    Add backend code which wipes expired events from db

--- backend/cloud/main.js
+++ backend/cloud/main.js
@@ -4,3 +4,29 @@
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+
+Parse.Cloud.job("nukeDeadEvents", function(request, status)
+{
+	Parse.Cloud.useMasterKey();
+
+
+	var query = new Parse.Query(Parse.Object.extend("Events"));
+	query.lessThan("expires", new Date());
+	query.find({
+		success: function(results)
+		{
+			Parse.Object.destroyAll(results).then(function(meh)
+			{
+				status.success("Job completed: " + results.length + " old events pruned");
+			});
+		},
+		error: function(error)
+		{
+			status.error("Failed to nuke old events: " + error.message);	
+		}
+	});
+});
+
+
+
