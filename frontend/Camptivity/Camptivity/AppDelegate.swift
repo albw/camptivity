72e14b6802aaaaeeaf698fdef4330875187e85b7
Khoa Nguyen <khoagng@gmail.com>
2015-02-03 11:08:43 -0800

    update bridging header

--- frontend/Camptivity/Camptivity/AppDelegate.swift
+++ frontend/Camptivity/Camptivity/AppDelegate.swift
@@ -7,17 +7,78 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        // Enable storing and querying data from Local Datastore.
+        // Remove this line if you don't want to use Local Datastore features or want to use cachePolicy.
+        Parse.enableLocalDatastore()
+
+        // ****************************************************************************
+        // Uncomment this line if you want to enable Crash Reporting
+        // ParseCrashReporting.enable()
+        //
+        // Uncomment and fill in with your Parse credentials:
+        Parse.setApplicationId("Y1fvAgliRdvCT1yXZBDNJtPm9QwMArNevFuWcqZm",
+            clientKey: "7JFUVHol8UfsNfLGxCzLoGHElKqp4KwLGski7o0N")
+        //
+        // If you are using Facebook, uncomment and add your FacebookAppID to your bundle's plist as
+        // described here: https://developers.facebook.com/docs/getting-started/facebook-sdk-for-ios/
+        // Uncomment the line inside ParseStartProject-Bridging-Header and the following line here:
+        // PFFacebookUtils.initializeFacebook()
+        // ****************************************************************************
+
+        PFUser.enableAutomaticUser()
+
+        let defaultACL = PFACL();
+
+        // If you would like all objects to be private by default, remove this line.
+        defaultACL.setPublicReadAccess(true)
+
+        PFACL.setDefaultACL(defaultACL, withAccessForCurrentUser:true)
+
+        if application.applicationState != UIApplicationState.Background {
+            // Track an app open here if we launch with a push, unless
+            // "content_available" was used to trigger a background push (introduced in iOS 7).
+            // In that case, we skip tracking here to avoid double counting the app-open.
+
+            let preBackgroundPush = !application.respondsToSelector("backgroundRefreshStatus")
+            let oldPushHandlerOnly = !self.respondsToSelector("application:didReceiveRemoteNotification:fetchCompletionHandler:")
+            var noPushPayload = false;
+            if let options = launchOptions {
+                noPushPayload = options[UIApplicationLaunchOptionsRemoteNotificationKey] != nil;
+            }
+            if (preBackgroundPush || oldPushHandlerOnly || noPushPayload) {
+                PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+            }
+        }
+        if application.respondsToSelector("registerUserNotificationSettings:") {
+            let userNotificationTypes = UIUserNotificationType.Alert | UIUserNotificationType.Badge | UIUserNotificationType.Sound
+            let settings = UIUserNotificationSettings(forTypes: userNotificationTypes, categories: nil)
+            application.registerUserNotificationSettings(settings)
+            application.registerForRemoteNotifications()
+        } else {
+            let types = UIRemoteNotificationType.Badge | UIRemoteNotificationType.Alert | UIRemoteNotificationType.Sound
+            application.registerForRemoteNotificationTypes(types)
+        }
+        
+        
+        
+        //Test Parse Insert
+//        var object = PFObject(className: "TestClass")
+//        object.addObject("Banana", forKey: "favoriteFood")
+//        object.addObject("Chocolate", forKey: "favoriteIceCream")
+//        object.saveInBackground()
+
+
         return true
     }
+
 
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
---
+++ frontend/Camptivity/Camptivity/Camptivity-Bridging-Header.h
@@ -0,0 +1,19 @@
+//
+//  Camptivity-Bridgin-Header.h
+//  Camptivity
+//
+//  Created by Khoa Nguyen on 1/31/15.
+//  Copyright (c) 2015 Shayan Mahdavi. All rights reserved.
+//
+
+#ifndef Camptivity_Camptivity_Bridgin_Header_h
+#define Camptivity_Camptivity_Bridgin_Header_h
+
+#import <Bolts/Bolts.h>
+#import <Parse/Parse.h>
+#import <GoogleMaps/GoogleMaps.h>
+
+// If you are using Facebook, uncomment this line to get automatic import of the header inside your project.
+//#import <ParseFacebookUtils/PFFacebookUtils.h>
+
+#endif
