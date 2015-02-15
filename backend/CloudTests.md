8d79037c3c057e29832a4bc70e3d92336470622d
fastily <fastily@yahoo.com>
2015-02-14 21:37:05 -0800

    Re-worked the entire backend to better fit our app's requirements.  Added new hooks for score, location averages, and user init

--- backend/CloudTests.md
+++ backend/CloudTests.md
@@ -1,15 +1,17 @@
-Sample CURL code to test Cloud Modules
+Sample cURL code to test Cloud Modules
 ------
 
-##### doAvg
+##### afterSave (LocationRank)
 
-Computes Average of 'rating' column in LocationRank
+Adds a test LocationRank object which will trigger following afterSave events: 
+* Re-compute rankAvg and save result in the Locations table
+* Increment user's locationReviews by one.
 
 ```bash
-curl -X POST \
+  curl -X POST \
   -H "X-Parse-Application-Id: Y1fvAgliRdvCT1yXZBDNJtPm9QwMArNevFuWcqZm" \
   -H "X-Parse-REST-API-Key: 8DzbViZ3uzuZVp9bZ9rztDQEKG0Tx9fP1HLPsx5U" \
   -H "Content-Type: application/json" \
-  -d '{"table":"LocationRank"}' \
-  https://api.parse.com/1/functions/doAvg
-  ```
+  -d '{"rating":7,"review":"hehe3", "target":{"__type": "Pointer", "className": "Locations", "objectId": "d70IYXni4G"}, "userID":{"__type": "Pointer", "className": "_User", "objectId": "DQioyBlFOJ"}}' \
+  https://api.parse.com/1/classes/LocationRank
+ ```
--- backend/cloud/main.js
+++ backend/cloud/main.js
@@ -1,48 +1,99 @@
+/* //////////////////////////////////////////////////////////////////////////////// */
+/* /////////////////////////// INTERNAL STANDBY /////////////////////////////////// */
+/* //////////////////////////////////////////////////////////////////////////////// */
+
+/**
+ * Performs initialization tasks for a new user:
+ * 1) Create a blank score row for user
+ */
+Parse.Cloud.afterSave("_User", function(request) {
+	if(!request.object)
+		console.log("Looks like we had an issue: " + JSON.stringify(request, null, 4));
+	else if(!request.object.existed()) {
+		var Score = Parse.Object.extend("Score");
+		var sc = new Score();
+		sc.save({userID: request.object, likesGiven: 0, likesReceived: 0, eventComments: 0, locationReviews: 0}).then(function(meh) {
+			console.log("Set-up score row for a new user!");
+		});
+	} 
+});
+
+/**
+ * Auto-sets numRankings and avgRank to 0 when new Locations object is created.
+ */
+Parse.Cloud.afterSave("Locations", function(request) {
+	if(!request.object)
+		console.log("Looks like we had an issue: " + JSON.stringify(request, null, 4));
+	else if(!request.object.existed())
+		request.object.save({numRankings: 0, avgRank: 0}).then(function(meh) {
+			console.log("Logged creation of new location object: " + request.object.get("name"));
+		});
+});
+
+
+/**
+ * Performs following functions: 
+ * 1) Re-computes the average for the Locations object referenced by this LocationRank. 
+ * 2) Increment user's locationReviews in Score table by one.
+ */ 
+Parse.Cloud.afterSave("LocationRank", function(request) {
+	if(request.object && !request.object.existed())
+		new Parse.Query("Locations").get(request.object.get("target").id, {
+			success: function(meh) {
+				meh.set("avgRank", (meh.get("numRankings") * meh.get("avgRank") + request.object.get("rating"))/(meh.get("numRankings") + 1));
+				meh.increment("numRankings");
+				meh.save().then(function(blah) {
+					console.log("Updated average of " + meh.get("name"));
+
+					new Parse.Query("Score").equalTo("userID", meh.get("userID")).first().then(function(obj) {
+							obj.increment("locationReviews");
+							obj.save().then(function(asdf) {
+								console.log("Incremented location review score for " + asdf.get("userID").id);
+							});
+					});
+				});
+			},
+			error: function(ex) {
+				console.log("OH NO: " + JSON.stringify(ex, null, 4 ));
+			}
+		});
+ });
+
+
+ /* //////////////////////////////////////////////////////////////////////////////// */
+ /* /////////////////////////////////// JOBS /////////////////////////////////////// */
+ /* //////////////////////////////////////////////////////////////////////////////// */
+
 
 /**
  * Internal job that goes through Events table and deletes all expired events.
  */
-Parse.Cloud.job("nukeDeadEvents", function(request, status)
-{
-	Parse.Cloud.useMasterKey();
-	new Parse.Query(Parse.Object.extend("Events")).lessThan("expires", new Date()).find({
-		success: function(results)
-		{
-			Parse.Object.destroyAll(results).then(function(meh)
-			{
-				status.success("Job completed: " + results.length + " old event(s) pruned");
-			});
-		},
-		error: function(error)
-		{
-			status.error("Failed to nuke old events: " + error.message);	
-		}
-	});
-});
+ Parse.Cloud.job("nukeDeadEvents", function(request, status) {
+ 	Parse.Cloud.useMasterKey();
+ 	new Parse.Query(Parse.Object.extend("Events")).lessThan("expires", new Date()).find({
+ 		success: function(results)
+ 		{
+ 			Parse.Object.destroyAll(results).then(function(meh)
+ 			{
+ 				status.success("Job completed: " + results.length + " old event(s) pruned");
+ 			});
+ 		},
+ 		error: function(error)
+ 		{
+ 			status.error("Failed to nuke old events: " + error.message);	
+ 		}
+ 	});
+ });
+
+
+ /* //////////////////////////////////////////////////////////////////////////////// */
+ /* /////////////////////////// CLOUD FUNCTIONS //////////////////////////////////// */
+ /* //////////////////////////////////////////////////////////////////////////////// */
+
 
 /**
- * Shitty compute average rankings function.  CAVEAT: Will give incorrect 
- * result if there are more than 1000 rankings. 
- *
- * Takes one parameter: a JSON object that contains a single element (table) keyed to 
- * either "EventRank" or "LocationRank" depending on which to calculate avg for:
- * (e.g. {"table":"LocationRank"})
+ * Test function.  Returns a String
  */
-Parse.Cloud.define("doAvg", function(request, response)
-{
-	new Parse.Query(Parse.Object.extend(request.params.table)).exists("rating").find({
-		success: function(result)
-		{
-			var x = 0;
-			for(var i = 0; i < result.length; i++)
-				x += result[i].get("rating");
-
-			response.success(x / result.length);
-
-		},
-		error: function(error)
-		{
-			response.error("Failed to compute average! " + error.message);
-		}
-	});
-});
+ Parse.Cloud.define("hello", function(request, response) {
+ 	response.success("Hello, world!");
+ });
