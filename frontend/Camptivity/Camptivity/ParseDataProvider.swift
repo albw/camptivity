35cd97697ebabf152c9e3ff4befc344f746fd517
Phuong Hong Mai <Phuong@Phuongs-MacBook-Pro.local>
2015-02-17 21:14:19 -0800

    Added fetchLocationsNearMe

--- frontend/Camptivity/Camptivity/ParseDataProvider.swift
+++ frontend/Camptivity/Camptivity/ParseDataProvider.swift
@@ -21,13 +21,40 @@ class ParseDataProvider {
             else {
                 NSLog("%@", error)
             }
-            
             completion(result:results)
         }
     }
     
     
-    
-    
-    
+    func fetchLocationsNearMe(completion: (returnValue: [AnyObject])->Void) {
+        PFCloud.callFunctionInBackground("locationsNearMe", withParameters: ["category":"restroom","lat":32.88293263160078,"lon":-117.2109485336882,"radius":40]) {
+            (objects: AnyObject!, error: NSError!) -> Void in
+            var results = []
+            if (error != nil) {
+                // Your error handling here
+            }
+            else {
+                results = objects as NSArray
+                completion(returnValue:results)
+                /*
+                //NSLog("Result: \(result) ")
+                println("===================")
+
+                for (var i=0; i<result.count; i++)
+                {
+                    println(result[i]["avgRank"] as Int)
+                    println(result[i]["category"] as String)
+                    println(result[i]["description"] as String)
+                    println(result[i]["location"] as PFGeoPoint)
+                    println(result[i]["name"] as String)
+                    println(result[i]["numRankings"] as Int)
+                    println(result[i]["userID"] as PFUser)
+                }
+                */
+
+            }
+        }
+
+    }
+
 }
