49fe953d5f2256ce34065c9544a6256c405296fd
Phuong Hong Mai <Phuong@Phuongs-MacBook-Pro.local>
2015-03-07 20:48:51 -0800

    test lookupLocationByCoord

--- frontend/Camptivity/Camptivity/ParseDataProvider.swift
+++ frontend/Camptivity/Camptivity/ParseDataProvider.swift
@@ -325,15 +325,32 @@ class ParseDataProvider {
     *      long - Double - The longitude of the coordinate.
     * Example: {"lat":32.883192, "lon":-117.240933}
     */
-    func lookupLocationByCoord(lat:Double, long:Double)->AnyObject {
+    func lookupLocationByCoord(lat: Float, long: Float)->AnyObject {
+        
+        
+        //var query = PFQuery(className:"Locations")
+        //query.whereKey("location", equalTo: PFGeoPoint(latitude: d1, longitude: d2))
+        //var result = query.findObjects()
         
 
-        var query = PFQuery(className:"Locations")
-        query.whereKey("location", equalTo: PFGeoPoint(latitude: lat, longitude: long))
-        var result = query.findObjects()
-        
         //let result: AnyObject! = PFCloud.callFunction("lookupLocationByCoord", withParameters: ["lat":lat, "long":long])
+        
+        var result = []
+        PFCloud.callFunctionInBackground("lookupLocationByCoord", withParameters: ["lat":lat, "long":long]) {
+            (objects: AnyObject!, error: NSError!) -> Void in
 
+            if (error != nil) {
+                // Your error handling here
+            }
+            else {
+                result = objects as NSArray
+                
+            }
+        }
+        
+
+        println(result.count)
+        println(result)
         return result;
     }
     
