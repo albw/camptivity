f4271fe5bb48a3b526949f830dc61b1339f16355
ShayanMahdavi <s2mahdav@ucsd.edu>
2015-03-11 16:37:27 -0700

    Changed colors to make app look more flat and organized

--- frontend/Camptivity/Camptivity/AppDelegate.swift
+++ frontend/Camptivity/Camptivity/AppDelegate.swift
@@ -26,9 +26,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         application.statusBarStyle = .LightContent
         UIApplication.sharedApplication().statusBarStyle = .LightContent
         
-        UITabBar.appearance().backgroundColor = UIColor.blackColor()
-        //UITabBar.appearance().tintColor = UIColor.blackColor()
-        UITabBar.appearance().selectedImageTintColor = UIColor.blueColor()
+        //rgb(52, 152, 219)
+        UITabBar.appearance().backgroundColor = UIColor(red: 52.0/255.0, green: 152.0/255.0, blue: 219.0/255.0, alpha: 1.0)
+        UITabBar.appearance().barTintColor = UIColor(red: 52.0/255.0, green: 152.0/255.0, blue: 219.0/255.0, alpha: 1.0)
+        UITabBar.appearance().selectedImageTintColor = UIColor.blackColor()
 
         
         Fabric.with([Crashlytics()])
