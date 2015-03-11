6026c9597d54eec28fcc2c5a8c026c1ac345665f
ShayanMahdavi <s2mahdav@ucsd.edu>
2015-03-11 15:29:31 -0700

    added skeleton classes for Whats nearby feature

---
+++ frontend/Camptivity/Camptivity/LocationTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  LocationTableViewCell.swift
+//  Camptivity
+//
+//  Created by Shayan Mahdavi on 3/11/15.
+//  Copyright (c) 2015 Camptivity INC. All rights reserved.
+//
+
+import UIKit
+
+class LocationTableViewCell: UITableViewCell {
+    
+    //name
+    //description
+    //AvgRank
+    //Image
+    //Category
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ frontend/Camptivity/Camptivity/LocationTableViewController.swift
@@ -0,0 +1,63 @@
+//
+//  LocationTableViewController.swift
+//  Camptivity
+//
+//  Created by Shayan Mahdavi on 3/11/15.
+//  Copyright (c) 2015 Camptivity INC. All rights reserved.
+//
+
+import UIKit
+
+class LocationTableViewController: UITableViewController {
+    
+    var locationData: [PFObject]!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //Query and populate locationData
+        //ParseLocations.getLocationsNearMe()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return 200
+    }
+    
+    /*override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        return feedCellAtIndexPath(indexPath)
+    }
+    
+    func locationCellAtIndexPath(indexPath: NSIndexPath) -> LocationTableViewCell{
+        let cell = tableView.dequeueReusableCellWithIdentifier("locationCell") as LocationTableViewCell
+        if(indexPath.row < 200){ //Currently hardcoded, need a way to determine the amount of events
+            setCellDisplay(cell, indexPath: indexPath)
+        }
+    }
+    
+    //Helper Function for setting all relevent cell displays
+    func setCellDisplay(cell:LocationTableViewCell, indexPath:NSIndexPath) {
+        cell.name_label =
+        cell.description_label =
+        cell.image_label =
+        cell.category_label =
+        cell.avgrank_label =
+    }*/
+
+}
